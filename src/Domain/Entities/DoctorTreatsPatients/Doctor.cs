6ad1b3fa4fde2bb8dacd63de9227db63769f87b8
Jaroslav Plšek <xplsek@fi.muni.cz>
2024-01-19 18:28:18 +0100

    feat start coding context

---
+++ src/Domain/Entities/DoctorTreatsPatients/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemForSharingInfoInHospitals.Domain.Entities.DoctorTreatsPatients;
+public class Doctor : BaseAuditableEntity
+{
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Degree { get; set; }
+
+    public IList<HoursPerDay> OfficeHours { get; set;} = new List<HoursPerDay>();
+
+
+
+}
---
+++ src/Domain/Entities/DoctorTreatsPatients/HoursPerDay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemForSharingInfoInHospitals.Domain.Entities.DoctorTreatsPatients;
+public class HoursPerDay: BaseAuditableEntity
+{
+    public int DoctorId { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeOnly Start { get; set; }
+    public TimeOnly End { get; set; }
+}
